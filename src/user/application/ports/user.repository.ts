b2481858da8fd948d4d52a6111ccb196f2ae68ff
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-04-16 12:23:08 -0300

    feat(user): add findByCriteria method to UserRepository port

--- src/user/application/ports/user.repository.ts
+++ src/user/application/ports/user.repository.ts
@@ -4,4 +4,7 @@ import { User } from '../../domain/entities/user.entity';
 @Injectable()
 export abstract class UserRepository {
   abstract save(user: User): Promise<void>;
+  abstract findByCriteria(
+    criteria: Pick<User, 'name' | 'email'> & { id: string },
+  ): Promise<User>;
 }
