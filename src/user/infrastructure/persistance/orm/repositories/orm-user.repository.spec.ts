a22d66d9437509f61c6fa38ecfe8dcf8f45585db
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-04-16 18:39:44 -0300

    refactor(orm-user-repository): replace real typeorm connection with mocked version

--- src/user/infrastructure/persistance/orm/repositories/orm-user.repository.spec.ts
+++ src/user/infrastructure/persistance/orm/repositories/orm-user.repository.spec.ts
@@ -1,40 +1,38 @@
 import { OrmUserRepository } from './orm-user.repository';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserFactory } from '../../../../domain/factories/user.factory';
-import { DataSourceOptions, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { OrmUser } from '../entities/orm-user.entity';
-import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { faker } from '@faker-js/faker';
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+const createMockRepository = <T = any>(): MockRepository<T> => ({
+  findOne: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn(),
+});
 
 describe('OrmUserRepository', () => {
   let sut: OrmUserRepository;
   let userFactory: UserFactory;
-  let userRepository: Repository<OrmUser>;
-
-  const options: DataSourceOptions = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'pass123',
-    database: 'travel-package-proposal-db',
-    synchronize: true,
-    entities: [OrmUser],
-  };
+  let typeOrmRepository: MockRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        TypeOrmModule.forRoot(options),
-        TypeOrmModule.forFeature([OrmUser]),
+      imports: [],
+      providers: [
+        OrmUserRepository,
+        UserFactory,
+        {
+          provide: getRepositoryToken(OrmUser),
+          useValue: createMockRepository(),
+        },
       ],
-      providers: [OrmUserRepository, UserFactory],
     }).compile();
     sut = module.get<OrmUserRepository>(OrmUserRepository);
     userFactory = module.get<UserFactory>(UserFactory);
-    userRepository = module.get<Repository<OrmUser>>(
-      getRepositoryToken(OrmUser),
-    );
+    typeOrmRepository = module.get<MockRepository>(getRepositoryToken(OrmUser));
   });
 
   describe('save()', () => {
@@ -47,9 +45,8 @@ describe('OrmUserRepository', () => {
 
       await sut.save(user);
 
-      const userSaved = await userRepository.findOne({
-        where: { email: user.email },
-      });
+      typeOrmRepository.findOne.mockReturnValue(user);
+      const userSaved = await sut.findByCriteria({ email: user.email });
       expect(userSaved).toBeTruthy();
     });
   });
@@ -59,6 +56,7 @@ describe('OrmUserRepository', () => {
       const email = faker.internet.email();
       const expectedUser = userFactory.create('Nobody', email, '12345678');
       await sut.save(expectedUser);
+      typeOrmRepository.findOne.mockReturnValue(expectedUser);
 
       const user = await sut.findByCriteria({ email });
 
