f3d49f22b1df9bf8ee66a75d71f191ca5df43929
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-04-19 14:19:54 -0300

    feat(authentication): ensure signup endpoint return 400 when passwords do not match

--- src/iam/authentication/dtos/sign-up.dto.ts
+++ src/iam/authentication/dtos/sign-up.dto.ts
@@ -1,6 +1,12 @@
+import { MatchesProperty } from '../../../common/decorators/validation/matches-property';
+
 export class SignUpDto {
   name: string;
+
   email: string;
+
   password: string;
+
+  @MatchesProperty('password', { message: 'Passwords do not match.' })
   confirmPassword: string;
 }
--- test/iam/authentication/authentication.e2e-spec.ts
+++ test/iam/authentication/authentication.e2e-spec.ts
@@ -1,12 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
-import { HttpStatus, INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
+import { faker } from '@faker-js/faker';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { IamModule } from '../../../src/iam/iam.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from '../../../src/user/application/ports/user.repository';
 import { SignUpDto } from '../../../src/iam/authentication/dtos/sign-up.dto';
-import { faker } from '@faker-js/faker';
 
 describe('Authentication (e2e)', () => {
   let app: INestApplication;
@@ -29,6 +29,7 @@ describe('Authentication (e2e)', () => {
       ],
     }).compile();
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
     await app.init();
     userRepository = moduleFixture.get<UserRepository>(UserRepository);
   });
@@ -56,6 +57,21 @@ describe('Authentication (e2e)', () => {
         email: dto.email,
       });
       expect(userExists).toBeTruthy();
+    });
+
+    test('SignUp fails when password and confirm password dont match', async () => {
+      const dto: SignUpDto = {
+        email: faker.internet.email(),
+        name: faker.person.fullName(),
+        password: faker.internet.password(),
+        confirmPassword: faker.internet.password(),
+      };
+
+      const { statusCode } = await request(app.getHttpServer())
+        .post('/authentication/sign-up')
+        .send(dto);
+
+      expect(statusCode).toBe(HttpStatus.BAD_REQUEST);
     });
   });
 });
