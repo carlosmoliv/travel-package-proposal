d0bc136f7bd149d74cde147b83a2a54922e4a72b
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-04-23 17:44:48 -0300

    feat(auth-service): ensure signIn generates the accessToken when user credentials are valid

--- src/iam/authentication/authentication.service.spec.ts
+++ src/iam/authentication/authentication.service.spec.ts
@@ -8,11 +8,14 @@ import { UserRepository } from '../../user/application/ports/user.repository';
 import { HashingService } from '../ports/hashing.service';
 import { SignUpPayload } from './payloads/sign-up.payload';
 import { UserFactory } from '../../user/domain/factories/user.factory';
+import { SignInPayload } from './payloads/sign-in.payload';
+import { TokenService } from '../ports/token.service';
 
 describe('AuthenticationService', () => {
   let sut: AuthenticationService;
   let userRepository: MockProxy<UserRepository>;
   let hashingService: MockProxy<HashingService>;
+  let tokenService: MockProxy<TokenService>;
   let userFactory: UserFactory;
 
   beforeEach(async () => {
@@ -22,11 +25,13 @@ describe('AuthenticationService', () => {
         UserFactory,
         { provide: UserRepository, useValue: mock() },
         { provide: HashingService, useValue: mock() },
+        { provide: TokenService, useValue: mock() },
       ],
     }).compile();
     sut = module.get<AuthenticationService>(AuthenticationService);
     userRepository = module.get<MockProxy<UserRepository>>(UserRepository);
     hashingService = module.get<MockProxy<HashingService>>(HashingService);
+    tokenService = module.get<MockProxy<TokenService>>(TokenService);
     userFactory = module.get<UserFactory>(UserFactory);
   });
 
@@ -44,7 +49,7 @@ describe('AuthenticationService', () => {
     test('Registration of a new user', async () => {
       // Arrange
       userRepository.findByCriteria.mockResolvedValue(undefined);
-      hashingService.hash.mockResolvedValue('hashedPassword');
+      hashingService.hash.mockResolvedValue('hashed_password');
 
       // Act
       await sut.signUp(payload);
@@ -52,23 +57,50 @@ describe('AuthenticationService', () => {
       // Assert
       expect(userRepository.save).toHaveBeenCalledWith({
         ...payload,
-        password: 'hashedPassword',
+        id: expect.any(String),
+        password: 'hashed_password',
       });
     });
 
     test('Registration throws an exception when the email provided is already in use', async () => {
       // Arrange
       userRepository.findByCriteria.mockResolvedValue(
+        userFactory.create('any_name', payload.email, payload.password),
+      );
+      hashingService.hash.mockResolvedValue('hashed_password');
+
+      // Act and Assert
+      await expect(sut.signUp(payload)).rejects.toThrow(ConflictException);
+    });
+  });
+
+  describe('signIn()', () => {
+    let payload: SignInPayload;
+
+    beforeEach(() => {
+      payload = {
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+      };
+    });
+
+    test('Authenticate User and return the accessToken', async () => {
+      // Arrange
+      userRepository.findByCriteria.mockResolvedValue(
         userFactory.create(
-          faker.person.fullName(),
+          'any_id',
+          'any_name',
           payload.email,
           payload.password,
         ),
       );
-      hashingService.hash.mockResolvedValue('hashedPassword');
+      tokenService.generate.mockResolvedValue('generated_token');
 
-      // Act and Assert
-      await expect(sut.signUp(payload)).rejects.toThrow(ConflictException);
+      // Act
+      const accessToken = await sut.signIn(payload);
+
+      // Assert
+      expect(accessToken).toBe('generated_token');
     });
   });
 });
--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -1,8 +1,14 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { SignUpPayload } from './payloads/sign-up.payload';
 import { UserRepository } from '../../user/application/ports/user.repository';
 import { HashingService } from '../ports/hashing.service';
 import { UserFactory } from '../../user/domain/factories/user.factory';
+import { SignInPayload } from './payloads/sign-in.payload';
+import { TokenService } from '../ports/token.service';
 
 @Injectable()
 export class AuthenticationService {
@@ -10,6 +16,7 @@ export class AuthenticationService {
     private readonly userRepository: UserRepository,
     private readonly hashingService: HashingService,
     private readonly userFactory: UserFactory,
+    private readonly tokenService: TokenService,
   ) {}
 
   async signUp(payload: SignUpPayload): Promise<void> {
@@ -19,5 +26,14 @@ export class AuthenticationService {
     const hashedPassword = await this.hashingService.hash(password);
     const user = this.userFactory.create(name, email, hashedPassword);
     await this.userRepository.save(user);
+  }
+
+  async signIn(payload: SignInPayload) {
+    const { email } = payload;
+    const user = await this.userRepository.findByCriteria({
+      email: payload.email,
+    });
+    if (!user) throw new UnauthorizedException('User does not exists');
+    return this.tokenService.generate({ userId: user.id, email }, 3600);
   }
 }
