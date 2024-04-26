b44cd7b0979d6660a73694d3ef50e4e1bd9aa13c
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-04-26 14:27:53 -0300

    chore: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const options = new DocumentBuilder()
+    .setTitle('Travel Package Proposal API')
+    .setDescription('Project endpoints.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
