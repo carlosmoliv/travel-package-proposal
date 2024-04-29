07f073485677af314eb45369891791bab7ad9693
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-04-29 10:56:25 -0300

    feat(refresh-token-ids-storage): validate token returns returns false and true

--- src/iam/authentication/refresh-token-ids.storage/refresh-token-ids.storage.spec.ts
+++ src/iam/authentication/refresh-token-ids.storage/refresh-token-ids.storage.spec.ts
@@ -19,14 +19,39 @@ describe('RefreshTokenIdsStorageService', () => {
     storageService = module.get<MockProxy<StorageService>>(StorageService);
   });
 
-  test('Insert token with correct key into storage', async () => {
-    const userId = '123';
-    const tokenId = 'abc123';
-    const expectedKey = 'user-123';
+  describe('insert()', () => {
+    test('Insert token with correct key into storage', async () => {
+      const userId = '123';
+      const tokenId = 'abc123';
+      const expectedKey = 'user-123';
 
-    await sut.insert(userId, tokenId);
+      await sut.insert(userId, tokenId);
 
-    expect(storageService.set).toHaveBeenCalledTimes(1);
-    expect(storageService.set).toHaveBeenCalledWith(expectedKey);
+      expect(storageService.set).toHaveBeenCalledWith(expectedKey, tokenId);
+      expect(storageService.set).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('validate()', () => {
+    test('Validate returns true if token is valid', async () => {
+      const userId = '123';
+      const tokenId = 'abc123';
+      storageService.get.mockResolvedValue(tokenId);
+
+      const result = await sut.validate(userId, tokenId);
+
+      expect(result).toBe(true);
+    });
+
+    test('Validate returns false if token is invalid', async () => {
+      const userId = '123';
+      const tokenId = 'abc123';
+      const invalidTokenId = 'invalid_token';
+      storageService.get.mockResolvedValue(tokenId);
+
+      const result = await sut.validate(userId, invalidTokenId);
+
+      expect(result).toBe(false);
+    });
   });
 });
--- src/iam/authentication/refresh-token-ids.storage/refresh-token-ids.storage.ts
+++ src/iam/authentication/refresh-token-ids.storage/refresh-token-ids.storage.ts
@@ -10,6 +10,11 @@ export class RefreshTokenIdsStorage {
     await this.storageService.set(this.getKey(userId), tokenId);
   }
 
+  async validate(userId: string, tokenId: string): Promise<boolean> {
+    const storedId = await this.storageService.get(this.getKey(userId));
+    return storedId === tokenId;
+  }
+
   private getKey(userId: string): string {
     return `user-${userId}`;
   }
