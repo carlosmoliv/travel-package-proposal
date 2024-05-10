658a0188b36f64332eec67b3116bf7c3037e3df8
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-05-10 11:51:46 -0300

    refactor(auth-guard): organize code

--- src/iam/authentication/guards/authentication/authentication.guard.ts
+++ src/iam/authentication/guards/authentication/authentication.guard.ts
@@ -13,16 +13,16 @@ export class AuthenticationGuard implements CanActivate {
   constructor(private readonly tokenService: TokenService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const token = this.extractTokenFromHeader(request);
-    if (!token) throw new UnauthorizedException();
     try {
+      const request = context.switchToHttp().getRequest();
+      const token = this.extractTokenFromHeader(request);
+      if (!token) throw new Error('Token is missing.');
       const payload = await this.tokenService.validateAndDecode(token);
       request['user'] = payload;
+      return true;
     } catch {
       throw new UnauthorizedException();
     }
-    return true;
   }
 
   private extractTokenFromHeader(request: Request): string | undefined {
