2837c7d6d1cf8b4d18d795f28140f2b8337c2324
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-05-13 11:46:12 -0300

    feat(user): add UserService and UserController to the UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,6 +4,8 @@ import { OrmUserRepository } from './infrastructure/persistance/orm/repositories
 import { UserFactory } from './domain/factories/user.factory';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrmUser } from './infrastructure/persistance/orm/entities/orm-user.entity';
+import { UserController } from './presenters/controllers/user.controller';
+import { UserService } from './application/user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([OrmUser])],
@@ -13,7 +15,9 @@ import { OrmUser } from './infrastructure/persistance/orm/entities/orm-user.enti
       provide: UserRepository,
       useClass: OrmUserRepository,
     },
+    UserService,
   ],
+  controllers: [UserController],
   exports: [UserRepository, UserFactory],
 })
 export class UserModule {}
