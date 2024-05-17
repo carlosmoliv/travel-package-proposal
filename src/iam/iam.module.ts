4d118f70bf5fe572ea8d767f531c3f74d14d41be
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-05-17 14:37:41 -0300

    refactor(iam-module): organize imports

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_GUARD } from '@nestjs/core';
 
 import { BcryptService } from './hashing/bcrypt/bcrypt.service';
 import { HashingService } from './ports/hashing.service';
@@ -9,15 +11,13 @@ import { AuthenticationController } from './authentication/authentication.contro
 import { UserModule } from '../user/user.module';
 import { JwtService } from './token/jwt/jwt.service';
 import { TokenService } from './ports/token.service';
-import jwtConfig from './token/jwt/jwt.config';
-import iamConfig from './iam.config';
 import { RefreshTokenIdsStorage } from './authentication/refresh-token-ids.storage/refresh-token-ids.storage';
 import { SharedModule } from '../shared/shared.module';
 import { AuthenticationGuard } from './authentication/guards/authentication/authentication.guard';
-import { APP_GUARD } from '@nestjs/core';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrmPermission } from './authorization/persistence/orm/entities/permission.entity';
 import { OrmRole } from '../user/infrastructure/persistance/orm/entities/orm-role.entity';
+import jwtConfig from './token/jwt/jwt.config';
+import iamConfig from './iam.config';
 
 @Module({
   imports: [
