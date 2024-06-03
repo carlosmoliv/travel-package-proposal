8a9848763c71d577306371525de8df032a7c2550
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-06-03 12:16:52 -0300

    feat(permissions): add to save method to PermissionsRepository port

--- src/iam/authorization/application/ports/permissions.repository.ts
+++ src/iam/authorization/application/ports/permissions.repository.ts
@@ -2,4 +2,5 @@ import { Permission } from '../../domain/permission';
 
 export abstract class PermissionsRepository {
   abstract findByRoles(roleIds: string[]): Promise<Permission[]>;
+  abstract save(permission: Permission): Promise<void>;
 }
