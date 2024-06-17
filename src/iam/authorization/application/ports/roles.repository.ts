ffed44be80253aa3338631b2486640878948e5a5
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-06-17 11:31:15 -0300

    fet: add findByIds to RolesRepository port

--- src/iam/authorization/application/ports/roles.repository.ts
+++ src/iam/authorization/application/ports/roles.repository.ts
@@ -3,4 +3,5 @@ import { Role } from '../../domain/role';
 export abstract class RolesRepository {
   abstract save(role: Role): Promise<void>;
   abstract findById(id: string): Promise<Role>;
+  abstract findByIds(id: string[]): Promise<Role[]>;
 }
