3ba809f71a390e81878568c8e5842c705f98436c
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-07-29 11:36:40 -0300

    feat(client): add proposals to Client domain entity

--- src/client/domain/client.ts
+++ src/client/domain/client.ts
@@ -1,7 +1,10 @@
+import { Proposal } from '../../proposal/domain/proposal';
+
 export class Client {
   constructor(
     public userId: string,
     public contactInfo: string,
     public preferences?: string,
+    public bookings?: Proposal[],
   ) {}
 }
