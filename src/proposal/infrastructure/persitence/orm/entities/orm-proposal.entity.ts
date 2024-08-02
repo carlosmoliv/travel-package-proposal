dc90bd7646ad74f6071a60cf00143f722a08ba19
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-08-02 12:00:21 -0300

    fix(proposal): make orm entity extends OrmBaseEntity

--- src/proposal/infrastructure/persitence/orm/entities/orm-proposal.entity.ts
+++ src/proposal/infrastructure/persitence/orm/entities/orm-proposal.entity.ts
@@ -3,9 +3,10 @@ import { Column, Entity, JoinTable, ManyToMany, ManyToOne } from 'typeorm';
 import { OrmClient } from '../../../../../client/infrastructure/persitence/orm/entities/orm-client.entity';
 import { ProposalStatus } from '../../../../domain/enums/proposal-status';
 import { OrmTravelPackage } from '../../../../../travel-package/infrastructure/persistence/orm/entities/orm-travel-package.entity';
+import { OrmBaseEntity } from '../../../../../shared/infrastructure/persistence/orm/entities/orm-base.entity';
 
 @Entity('proposals')
-export class OrmProposal {
+export class OrmProposal extends OrmBaseEntity {
   @ManyToOne(() => OrmClient, (client) => client.bookings)
   client: OrmClient;
 
