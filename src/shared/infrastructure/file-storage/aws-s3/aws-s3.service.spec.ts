a6ee49a7326fc2ade103a06ba54e9631c965528c
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-08-17 15:57:15 -0300

    feat(aws-s3-service): ensure it handles file uploads to AWS S3

--- src/shared/infrastructure/file-storage/aws-s3/aws-s3.service.spec.ts
+++ src/shared/infrastructure/file-storage/aws-s3/aws-s3.service.spec.ts
@@ -1,9 +1,13 @@
+import 'aws-sdk-client-mock-jest';
+import { mockClient } from 'aws-sdk-client-mock';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { AwsS3Service } from './aws-s3.service';
 import awsS3Config from './aws-s3.config';
-import { S3Client } from '@aws-sdk/client-s3';
 
-jest.mock('@aws-sdk/client-s3');
+const s3Mock = mockClient(S3Client);
 
 describe('AwsS3Service', () => {
   let sut: AwsS3Service;
@@ -16,6 +20,7 @@ describe('AwsS3Service', () => {
           provide: awsS3Config.KEY,
           useValue: {
             s3Region: 'any_region',
+            bucketName: 'any_bucket_name',
           },
         },
       ],
@@ -24,9 +29,30 @@ describe('AwsS3Service', () => {
     sut = module.get<AwsS3Service>(AwsS3Service);
   });
 
-  test('should config aws credentials on creation', async () => {
+  afterEach(() => {
+    s3Mock.reset();
+  });
+
+  test('should config aws credentials on creation', () => {
     expect(S3Client).toHaveBeenCalledWith({
       region: 'any_region',
+    });
+  });
+
+  describe('upload()', () => {
+    test('upload file to S3 storage', async () => {
+      const fileBuffer = Buffer.from('file content');
+      const fileName = 'test-file.txt';
+
+      s3Mock.on(PutObjectCommand).resolves({});
+
+      await sut.upload(fileBuffer, fileName);
+
+      expect(s3Mock).toHaveReceivedCommandWith(PutObjectCommand, {
+        Bucket: 'any_bucket_name',
+        Key: fileName,
+        Body: fileBuffer,
+      });
     });
   });
 });
--- src/shared/infrastructure/file-storage/aws-s3/aws-s3.service.ts
+++ src/shared/infrastructure/file-storage/aws-s3/aws-s3.service.ts
@@ -1,10 +1,13 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+
 import awsS3Config from './aws-s3.config';
-import { S3Client } from '@aws-sdk/client-s3';
+import { FileStorageService } from '../../../application/ports/file-storage.service';
 
 @Injectable()
-export class AwsS3Service {
+export class AwsS3Service implements FileStorageService {
   private readonly s3Client: S3Client;
 
   constructor(
@@ -14,5 +17,15 @@ export class AwsS3Service {
     this.s3Client = new S3Client({
       region: this.awsConfiguration.s3Region,
     });
+  }
+
+  async upload(file: Buffer, fileName: string): Promise<void> {
+    await this.s3Client.send(
+      new PutObjectCommand({
+        Bucket: this.awsConfiguration.bucketName,
+        Key: fileName,
+        Body: file,
+      }),
+    );
   }
 }
