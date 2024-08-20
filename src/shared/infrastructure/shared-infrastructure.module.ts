1da20ab87dbb88a3fb644e5df7035fd75512e78b
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-08-20 11:30:23 -0300

    feat: update SharedInfrastructureModule

--- src/shared/infrastructure/shared-infrastructure.module.ts
+++ src/shared/infrastructure/shared-infrastructure.module.ts
@@ -3,11 +3,20 @@ import { ConfigModule } from '@nestjs/config';
 
 import { RedisService } from './redis/redis.service';
 import { StorageService } from '../application/ports/storage.service';
+import { AwsS3Service } from './file-storage/aws-s3/aws-s3.service';
 import redisConfig from './redis/redis.config';
+import { FileStorageService } from '../application/ports/file-storage.service';
+import awsS3Config from './file-storage/aws-s3/aws-s3.config';
 
 @Module({
-  imports: [ConfigModule.forFeature(redisConfig)],
-  providers: [{ provide: StorageService, useClass: RedisService }],
-  exports: [StorageService],
+  imports: [
+    ConfigModule.forFeature(redisConfig),
+    ConfigModule.forFeature(awsS3Config),
+  ],
+  providers: [
+    { provide: StorageService, useClass: RedisService },
+    { provide: FileStorageService, useClass: AwsS3Service },
+  ],
+  exports: [StorageService, FileStorageService],
 })
 export class SharedInfrastructureModule {}
