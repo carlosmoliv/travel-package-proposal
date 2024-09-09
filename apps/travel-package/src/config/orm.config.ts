318716ef64415dcd21a093286fe8d7807ae87c05
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-09-09 11:15:51 -0300

    feat(travel-package): dockerize travel-package microservice

---
+++ apps/travel-package/src/config/orm.config.ts
@@ -0,0 +1,31 @@
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
+export const ormConfig = {
+  type: 'postgres' as const,
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+  username: process.env.DATABASE_USER || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'pass123',
+  database: process.env.DATABASE_NAME || 'travel-packaged-db',
+  synchronize: true,
+  logging: false,
+  migrationsTableName: 'typeorm_migrations',
+  autoLoadEntities: true,
+  // entities: ['dist/**/*.entity.{ts,js}'],
+  migrations: ['dist/migrations/*.{ts,js}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
+
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+  useFactory: async (): Promise<TypeOrmModuleOptions> => {
+    return ormConfig;
+  },
+};
+
+export default new DataSource(ormConfig);
