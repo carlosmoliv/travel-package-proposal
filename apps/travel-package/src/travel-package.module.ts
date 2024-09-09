b481b375177226a094ed206f415aa6e80f7422d4
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-09-09 11:15:03 -0300

    feat(travel-package): update module

--- apps/travel-package/src/travel-package.module.ts
+++ apps/travel-package/src/travel-package.module.ts
@@ -4,12 +4,20 @@ import { TravelPackageController } from './presenters/controllers/travel-package
 import { TravelPackageService } from './application/travel-package.service';
 import { TravelPackageRepository } from './application/ports/travel-package.repository';
 import { OrmTravelPackageRepository } from './infrastructure/persistence/orm/repositories/orm-travel-package.repository';
+import { TravelPackageFactory } from './domain/factories/travel-package.factory';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrmTravelPackage } from './infrastructure/persistence/orm/entities/orm-travel-package.entity';
+import { typeOrmAsyncConfig } from './config/orm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    TypeOrmModule.forFeature([OrmTravelPackage]),
+  ],
   controllers: [TravelPackageController],
   providers: [
     TravelPackageService,
+    TravelPackageFactory,
     {
       provide: TravelPackageRepository,
       useClass: OrmTravelPackageRepository,
