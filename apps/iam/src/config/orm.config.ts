35d833fcee18e109b95859d28b9a7dd4ecaeaff3
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-09-18 13:47:25 -0300

    chore: update docker-compose

--- apps/iam/src/config/orm.config.ts
+++ apps/iam/src/config/orm.config.ts
@@ -10,7 +10,7 @@ export const ormConfig = {
   port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
   username: process.env.DATABASE_USER || 'postgres',
   password: process.env.DATABASE_PASSWORD || 'pass123',
-  database: process.env.DATABASE_NAME || 'travel-package-proposal-db',
+  database: process.env.DATABASE_NAME || 'iam-db',
   synchronize: true,
   logging: false,
   migrationsTableName: 'typeorm_migrations',
