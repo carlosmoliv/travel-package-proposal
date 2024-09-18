974ee9d13ecdff17b6642711dd17536d75550cca
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-09-18 14:39:56 -0300

    fix: replace bcrypt with bcryptjs to in containerized environment

--- apps/iam/src/shared/hashing/bcrypt/bcrypt.service.ts
+++ apps/iam/src/shared/hashing/bcrypt/bcrypt.service.ts
@@ -1,4 +1,4 @@
-import { genSalt, hash, compare } from 'bcrypt';
+import { genSalt, hash, compare } from 'bcryptjs';
 
 import { Injectable } from '@nestjs/common';
 
@@ -6,12 +6,12 @@ import { HashingService } from '../hashing.service';
 
 @Injectable()
 export class BcryptService implements HashingService {
-  async hash(data: string | Buffer): Promise<string> {
+  async hash(data: string): Promise<string> {
     const salt = await genSalt();
     return hash(data, salt);
   }
 
-  compare(data: string | Buffer, encrypted: string): Promise<boolean> {
+  compare(data: string, encrypted: string): Promise<boolean> {
     return compare(data, encrypted);
   }
 }
