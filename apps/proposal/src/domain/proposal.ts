4617c9c016dafe36601a75d7f19ad1e312d0246a
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-09-20 12:45:39 -0300

    feat(proposal): add Proposal entity

---
+++ apps/proposal/src/domain/proposal.ts
@@ -0,0 +1,11 @@
+import { ProposalStatus } from './enums/proposal-status';
+
+export class Proposal {
+  constructor(
+    public id: string,
+    public clientId: string,
+    public travelAgentId: string,
+    public travelPackageId: string,
+    public status: ProposalStatus,
+  ) {}
+}
