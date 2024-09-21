19f2fb03615590a9ce0b9d07874c4ddaa0e7d990
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-09-21 11:49:35 -0300

    feat: add Permission for proposal creation

---
+++ libs/common/src/iam/enums/proposal.permissions.enum.ts
@@ -0,0 +1,3 @@
+export enum ProposalPermission {
+  CreateProposal = 'create_proposal',
+}
--- libs/common/src/iam/permission.type.ts
+++ libs/common/src/iam/permission.type.ts
@@ -2,9 +2,11 @@ import { TravelPackagePermission } from '@app/common/iam/enums/travel-package.pe
 import { ExamplePermission } from '@app/common/iam/enums/example-permission.enum';
 import { UserPermission } from '@app/common/iam/enums/user.permissions';
 import { RolePermission } from '@app/common/iam/enums/role.permissions';
+import { ProposalPermission } from '@app/common/iam/enums/proposal.permissions.enum';
 
 export type PermissionType =
   | ExamplePermission
   | UserPermission
   | RolePermission
-  | TravelPackagePermission;
+  | TravelPackagePermission
+  | ProposalPermission;
