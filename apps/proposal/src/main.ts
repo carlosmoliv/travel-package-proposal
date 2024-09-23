a1bfdf445d6365911dbce892126814766d966a26
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-09-23 11:25:58 -0300

    fix(proposal): dockerize application

--- apps/proposal/src/main.ts
+++ apps/proposal/src/main.ts
@@ -3,6 +3,6 @@ import { ProposalModule } from './proposal.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ProposalModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
