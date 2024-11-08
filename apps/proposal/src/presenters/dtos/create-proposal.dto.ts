bd25dfc9c585dc452dffe6e77d5e478fdd7b834d
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-11-08 17:42:45 -0300

    fix(proposal): add missing price property to CreateProposalDto

--- apps/proposal/src/presenters/dtos/create-proposal.dto.ts
+++ apps/proposal/src/presenters/dtos/create-proposal.dto.ts
@@ -1,4 +1,4 @@
-import { IsString } from 'class-validator';
+import { IsDecimal, IsString } from 'class-validator';
 
 export class CreateProposalDto {
   @IsString()
@@ -9,4 +9,7 @@ export class CreateProposalDto {
 
   @IsString()
   travelPackageId: string;
+
+  @IsDecimal()
+  price: number;
 }
