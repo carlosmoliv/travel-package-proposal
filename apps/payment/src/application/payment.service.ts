a13dd1c541750217501afbde0e1a39b00841d2cc
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-11-17 14:31:32 -0300

    fix(payment): uncomment code

--- apps/payment/src/application/payment.service.ts
+++ apps/payment/src/application/payment.service.ts
@@ -1,20 +1,28 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 
 import { PaymentGatewayService } from './ports/payment-gateway.service';
 import { CreatePaymentInput } from './inputs/create-payment.input';
 
 @Injectable()
 export class PaymentService {
+  private readonly logger = new Logger(PaymentService.name);
+
   constructor(private readonly paymentGateway: PaymentGatewayService) {}
 
   async create(input: CreatePaymentInput) {
-    throw new BadRequestException();
-    // try {
-    //   const chargeResult = await this.paymentGateway.createCharge(input.amount);
-    //
-    //   return { referenceId: chargeResult.referenceId };
-    // } catch (error) {
-    //   throw new BadRequestException('Payment failed');
-    // }
+    try {
+      const chargeResult = await this.paymentGateway.createCharge(input.amount);
+      return { referenceId: chargeResult.referenceId };
+    } catch (error) {
+      this.logger.error(
+        `Failed to create payment. Input: ${JSON.stringify(input)}`,
+        error.stack,
+      );
+      throw new InternalServerErrorException('Payment failed');
+    }
   }
 }
