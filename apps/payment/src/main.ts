cee8831ee6d25c62529cc10c2f256139ff79ceb5
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-11-17 14:15:24 -0300

    feat(payment): configure RMQ as transport

--- apps/payment/src/main.ts
+++ apps/payment/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { PaymentModule } from './payment.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(PaymentModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    PaymentModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URI],
+        queue: 'payment_queue',
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
