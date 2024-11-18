0a7df7be514604fc82bfc0d0b54f841aa6e38fd9
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-11-18 14:41:12 -0300

    fix(proposal): implement accept and pay proposal in ProposalController

--- apps/proposal/src/presenters/controllers/proposal.controller.ts
+++ apps/proposal/src/presenters/controllers/proposal.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 
 import { TravelPackagePermission } from '@app/common/iam/enums/travel-package.permissions.enum';
 import { Permissions } from '@app/common/iam/decorators/permissions.decorator';
@@ -11,9 +18,20 @@ export class ProposalController {
   constructor(private readonly proposalService: ProposalService) {}
 
   @Permissions(TravelPackagePermission.CreateTravelPackage)
-  @HttpCode(HttpStatus.CREATED)
   @Post()
   async create(@Body() createProposalDto: CreateProposalDto) {
     await this.proposalService.create(createProposalDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post(':proposalId/accept')
+  async acceptProposal(@Param('proposalId') proposalId: string): Promise<void> {
+    await this.proposalService.acceptProposal(proposalId);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post(':proposalId/pay')
+  async payProposal(@Param('payment') proposalId: string): Promise<void> {
+    await this.proposalService.payProposal(proposalId);
   }
 }
