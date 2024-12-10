c6e965daf76d2cd89e4c8137647efeaa9725af33
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-12-10 10:36:08 -0300

    fix(travel-package): update entity name

--- apps/travel-package/src/travel-package.module.ts
+++ apps/travel-package/src/travel-package.module.ts
@@ -15,7 +15,7 @@ import { TravelPackageService } from './application/travel-package.service';
 import { TravelPackageRepository } from './application/ports/travel-package.repository';
 import { OrmTravelPackageRepository } from './infrastructure/persistence/orm/repositories/orm-travel-package.repository';
 import { TravelPackageFactory } from './domain/factories/travel-package.factory';
-import { OrmTravelPackage } from './infrastructure/persistence/orm/entities/orm-travel-package.entity';
+import { TravelPackageEntity } from './infrastructure/persistence/orm/entities/travel-package.entity';
 import { typeOrmAsyncConfig } from './config/orm.config';
 
 @Module({
@@ -29,7 +29,7 @@ import { typeOrmAsyncConfig } from './config/orm.config';
       }),
     }),
     TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
-    TypeOrmModule.forFeature([OrmTravelPackage]),
+    TypeOrmModule.forFeature([TravelPackageEntity]),
     ClientsModule.register([
       {
         name: IAM_SERVICE,
