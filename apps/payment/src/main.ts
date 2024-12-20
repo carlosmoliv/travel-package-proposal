73007d1f6b2477149c3363486a1ad08db5a56860
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-12-20 14:03:28 -0300

    fix(payment): add missing file from previous commits

--- apps/payment/src/main.ts
+++ apps/payment/src/main.ts
@@ -3,16 +3,16 @@ import { PaymentModule } from './payment.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    PaymentModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: [process.env.RABBITMQ_URI],
-        queue: 'payment_queue',
-      },
+  const app = await NestFactory.create(PaymentModule, { rawBody: true });
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBITMQ_URI],
+      queue: 'payment_queue',
     },
-  );
-  await app.listen();
+  });
+  await app.listen(process.env.PORT);
+  await app.startAllMicroservices();
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
