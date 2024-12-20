2d78d8030387cb0e18cc2326b3f853a187a2594d
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-12-20 14:39:05 -0300

    fix(proposal): return checkout URL from payProposal controller

--- apps/proposal/src/presenters/controllers/proposal.controller.ts
+++ apps/proposal/src/presenters/controllers/proposal.controller.ts
@@ -7,31 +7,35 @@ import {
   Post,
 } from '@nestjs/common';
 
-import { TravelPackagePermission } from '@app/common/iam/enums/travel-package.permissions.enum';
 import { Permissions } from '@app/common/iam/decorators/permissions.decorator';
 
 import { CreateProposalDto } from '../dtos/create-proposal.dto';
 import { ProposalService } from '../../application/proposal.service';
+import { ProposalPermission } from '@app/common/iam/enums/proposal.permissions.enum';
 
 @Controller('proposals')
 export class ProposalController {
   constructor(private readonly proposalService: ProposalService) {}
 
-  @Permissions(TravelPackagePermission.CreateTravelPackage)
+  @Permissions(ProposalPermission.CreateProposal)
   @Post()
   async create(@Body() createProposalDto: CreateProposalDto) {
     await this.proposalService.create(createProposalDto);
   }
 
+  @Permissions(ProposalPermission.AcceptProposal)
   @HttpCode(HttpStatus.OK)
   @Post(':proposalId/accept')
   async acceptProposal(@Param('proposalId') proposalId: string): Promise<void> {
     await this.proposalService.acceptProposal(proposalId);
   }
 
+  @Permissions(ProposalPermission.PayProposal)
   @HttpCode(HttpStatus.OK)
   @Post(':proposalId/pay')
-  async payProposal(@Param('payment') proposalId: string): Promise<void> {
-    await this.proposalService.payProposal(proposalId);
+  async payProposal(
+    @Param('proposalId') proposalId: string,
+  ): Promise<{ checkoutUrl: string }> {
+    return this.proposalService.payProposal(proposalId);
   }
 }
