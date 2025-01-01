c7ae5a545c8c49d217498ac21322784b46ba71b3
Carlos Oliveira <carlosmoliveira93@gmail.com>
2025-01-01 11:59:13 -0300

    refactor(payment): add missing orm configuration file

---
+++ apps/payment/src/config/orm.config.ts
@@ -0,0 +1,30 @@
+import { DataSource } from 'typeorm';
+
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+
+export const ormConfig = {
+  type: 'postgres' as const,
+  url:
+    process.env.DATABASE_URL ||
+    'postgresql://postgres:pass123@payment-postgres-db:5432/payment-db',
+  synchronize: true,
+  logging: false,
+  migrationsTableName: 'typeorm_migrations',
+  autoLoadEntities: true,
+  // entities: ['dist/**/*.entity.{ts,js}'],
+  migrations: ['dist/migrations/*.{ts,js}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
+
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+  useFactory: async (): Promise<TypeOrmModuleOptions> => {
+    return ormConfig;
+  },
+};
+
+export default new DataSource(ormConfig);
