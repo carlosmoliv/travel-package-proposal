28ac10409946b762c35e894117e7484cccebb599
Carlos Oliveira <carlosmoliveira93@gmail.com>
2025-01-01 15:56:33 -0300

    feat(payment): add main configuration for payment microservice

--- apps/notification/src/main.ts
+++ apps/notification/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 import { NotificationModule } from './notification.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(NotificationModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    NotificationModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URI],
+        queue: 'notification_queue',
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
